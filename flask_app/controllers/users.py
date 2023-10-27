91efb0bed4c7ab7b931702af689b622fa0be3ae4
dervishixhoni <xdervishi21@epoka.edu.al>
2023-10-27 21:20:47 +0200

    users api url

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -235,7 +235,7 @@ def dashboard():
     loggedUser = User.get_user_by_id(data)
     if loggedUser["isVerified"] == 0:
         return redirect("/verify/email")
-    url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+    url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&sort_by=popularity.desc"
 
     headers = {
         "accept": "application/json",
